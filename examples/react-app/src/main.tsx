ed71d2e0acadbd889450426ac300e821b3b2a06f
7nohe <7nohe.urata@gmail.com>
2022-07-22 16:15:50 +0900

    feat: use generated code in example project

--- examples/react-app/src/main.tsx
+++ examples/react-app/src/main.tsx
@@ -2,9 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import { QueryClient, QueryClientProvider, useQuery } from '@tanstack/react-query'
+const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
   </React.StrictMode>
 )
