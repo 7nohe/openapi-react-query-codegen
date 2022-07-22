f40ab64d292677186a4ec02c3693b5856b0a59c3
7nohe <7nohe.urata@gmail.com>
2022-07-22 16:37:35 +0900

    chore: remove unused command options

--- src/cli.ts
+++ src/cli.ts
@@ -18,19 +18,9 @@ program
   .description("Generate React Query code based on OpenAPI")
   .requiredOption("-p, --path <path>", "Path to OpenAPI file")
   .option(
-    "-pv, --package-version [version]",
-    "Semver version to use for the generated package",
-    "1.0.0"
-  )
-  .option(
     "-o, --output-dir [directory]",
     "Directory to output the generated package",
     "openapi"
-  )
-  .option("-b, --base-url [url]", "Prefix every request with this url")
-  .option(
-    "-r, --replacer [oldString] [newString...]",
-    "Replace part(s) of any route's path with simple string replacements. Ex: `-r /api/v1 /api/v2` would replace the v1 with v2 in every route"
   )
   .parse();
 
