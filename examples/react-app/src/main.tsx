4c74add1d6e9e330437abdcc054bd0a05d1dce0b
Urata Daiki <7nohe@users.noreply.github.com>
2024-05-04 02:05:04 +0900

    chore: Remove prefetch function in main.tsx

--- examples/react-app/src/main.tsx
+++ examples/react-app/src/main.tsx
@@ -3,19 +3,12 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import { QueryClientProvider } from "@tanstack/react-query";
-import { prefetchUseDefaultServiceFindPets } from "../openapi/queries/prefetch";
 import { queryClient } from "./queryClient";
 
-async function PrefetchData() {
-  await prefetchUseDefaultServiceFindPets(queryClient);
-}
-
-PrefetchData().then(() => {
-  ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-    <React.StrictMode>
-      <QueryClientProvider client={queryClient}>
-        <App />
-      </QueryClientProvider>
-    </React.StrictMode>,
-  );
-});
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <React.StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
+  </React.StrictMode>,
+);
