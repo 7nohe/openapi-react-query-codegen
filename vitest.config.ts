73cdc702508e1e09c2021db20a46fb3925771263
Takehrio Tada <tada@takenyan.dev>
2026-01-23 15:16:30 +0900

    feat: change vitest timeout

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
+    testTimeout: 20000,
     coverage: {
       reporter: ["text", "json-summary", "json", "html"],
       exclude: ["src/cli.mts", "examples/**", "tests/**", "docs/**"],
